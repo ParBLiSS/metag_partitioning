9d818c2d9d023ca65ab99edf9bcf875bc5bebc5f
Chirag Jain <cjain7@gatech.edu>
2015-03-28 20:13:26 -0400

    Code refactoring

--- include/parallel_fastq_iterate.hpp
+++ include/parallel_fastq_iterate.hpp
@@ -109,35 +109,3 @@ void generateKmerVector(MPI_Comm comm, const std::string &filename,
 
   std::cerr << "[generateKmerVector] Rank : " << rank << " parsed " << localVector.size() << " kmers\n";
 }
-
-
-int main(int argc, char** argv)
-{
-  // Initialize the MPI library:
-  MPI_Init(&argc, &argv);
-
-  //Specify the fileName
-  std::string filename; 
-  if( argc == 2 ) {
-    filename = argv[1];
-  }
-  else {
-    std::cout << "Usage: mpirun -np 4 <executable> FASTQ_FILE\n";
-    return 1;
-  }
-
-  //Specify Kmer Type
-  const int kmerLength = 31;
-  typedef bliss::common::DNA AlphabetType;
-  typedef bliss::common::Kmer<kmerLength, AlphabetType, uint32_t> KmerType;
-
-  //Initialize the KmerVector
-  std::vector<KmerType> localVector;
-
-  //Populate localVector for each rank
-  generateKmerVector<KmerType, AlphabetType> (MPI_COMM_WORLD, filename, localVector); 
-
-  MPI_Finalize();   
-  return(0);
-}
-
