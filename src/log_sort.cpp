1f702fae8c654bd909ec04546d9c530a94dee528
r4d2 <patrick.flick@gmail.com>
2015-04-11 16:17:36 -0400

    parallel log(D_max)
    Former-commit-id: 6bf4692010273a366a1c0828ae3f93f47ad09898 [formerly d9d266bc9039fb689618dd315b5ad2471913f15a]
    Former-commit-id: 290f56026a4011c2c5ff7474b816e133f89e0496

--- src/log_sort.cpp
+++ src/log_sort.cpp
@@ -12,29 +12,16 @@
 
 #include <mxx/collective.hpp>
 #include <mxx/distribution.hpp>
+#include <mxx/reduction.hpp>
 
 //from external repository
 #include <timer.hpp>
 
 #include <sstream>
 
-
-
-int main(int argc, char** argv)
+// sequential log(D_max) implementation
+void cluster_reads_seq(const std::string& filename)
 {
-  // Initialize the MPI library:
-  MPI_Init(&argc, &argv);
-
-  //Specify the fileName
-  std::string filename;
-  if( argc > 1 ) {
-    filename = argv[1];
-  }
-  else {
-    std::cout << "Usage: mpirun -np 4 <executable> FASTQ_FILE\n";
-    return 1;
-  }
-
   //Specify Kmer Type
   const int kmerLength = 31;
   typedef bliss::common::DNA AlphabetType;
@@ -135,20 +122,19 @@ int main(int argc, char** argv)
         });
     layer_comparator<2, tuple_t> pc_comp;
 
-
     // scan and find new p
     auto begin = localVector.begin();
     auto end = localVector.end();
     std::vector<tuple_t> newtuples;
     bool done = true;
     // for each range/bucket
+    //
+    // exchange bucket min/start !?
+    //
     for(; begin != end; ) {
       // find bucket (linearly)
-      auto eqr = findRange(begin, end,*begin, pc_comp);
+      auto eqr = findRange(begin, end, *begin, pc_comp);
       auto min = std::get<1>(*eqr.first);
-
-      //std::cout << "####" << std::endl;
-      //std::cout << localVector << std::endl;
 
       //int i = eqr.first - begin;
       //int j = eqr.second - begin;
@@ -179,27 +165,16 @@ int main(int argc, char** argv)
       uint32_t prev_pn = min;
       for (auto it = eqr.first+1; it != eqr.second; ++it) {
         uint32_t next_pn = std::get<1>(*it);
-        if (std::get<1>(*it) == prev_pn) {
-          if (!found_flip)
-          {
-            // set flipped
-            //std::cout << "Found repeat (" << std::get<1>(*it) << "," << std::get<2>(*it) << ")" << std::endl;
-            found_flip = true;
-            std::get<1>(*it) = std::get<2>(*it);
-            std::get<2>(*it) = min;
-          } else {
-            // moves over to new partition (since duplicates are not needed)
-            std::get<1>(*it) = min;
-            std::get<2>(*it) = min;
-          }
-        } else if (std::get<1>(*it) == std::get<2>(*it)) {
+        // if repeat or both entries equal:
+        if (std::get<1>(*it) == prev_pn || std::get<1>(*it) == std::get<2>(*it)) {
           if (!found_flip) {
-            //std::cout << "Found eq (" << std::get<1>(*it) << "," << std::get<2>(*it) << ")" << std::endl;
+            // set flipped
+            //std::cout << "Found repeat/eq (" << std::get<1>(*it) << "," << std::get<2>(*it) << ")" << std::endl;
             found_flip = true;
             std::get<1>(*it) = std::get<2>(*it);
             std::get<2>(*it) = min;
           } else {
-            // moves over to new partition
+            // moves over to new partition (since duplicates/equals are not needed)
             std::get<1>(*it) = min;
             std::get<2>(*it) = min;
           }
@@ -220,50 +195,14 @@ int main(int argc, char** argv)
         tuple_t t = *eqr.first;
         std::swap(std::get<1>(t),std::get<2>(t));
         newtuples.push_back(t);
-
         std::cout << "no flip found!" << std::endl;
-        //exit(EXIT_FAILURE);
-//        assert(false);
       }
 
       // next range
       begin = eqr.second;
-
     }
     keepGoing = !done;
     localVector.insert(localVector.end(), newtuples.begin(), newtuples.end());
-    /*
-    // sort by (P_c,P_n) and update P_c via P_n
-    mxx::sort(localVector.begin(), localVector.end(), // TODO: only active partitions
-        [](const tuple_t& x, const tuple_t&y){
-        return (std::get<2>(x) < std::get<2>(x)
-            || (std::get<2>(x) == std::get<2>(y)
-            && std::get<1>(x) < std::get<1>(y));
-        }
-        , MPI_COMM_WORLD, false);
-
-    // reduce over tuple
-
-    // sort by P_c and update P_c via P_n
-    mxx::sort(start, pend, layer_comparator<2, tuple_t>(), MPI_COMM_WORLD, true);
-    PartitionReducerType r2;
-    r2(start, pend, MPI_COMM_WORLD);
-    //sortAndReduceTuples<2, PartitionReducerType, tuple_t> (start, pend, MPI_COMM_WORLD);
-    MP_TIMER_END_SECTION("iteration PARTITION phase completed");
-
-    // check for global termination
-    keepGoing = !checkTermination<1, tuple_t>(start, pend, MPI_COMM_WORLD);
-    MP_TIMER_END_SECTION("iteration Check phase completed");
-
-    if (keepGoing) {
-      // now reduce to only working with active partitions
-      pend = std::partition(start, pend, app);
-      MP_TIMER_END_SECTION("iteration std::partition completed");
-      // re-shuffle the partitions to counter-act the load-inbalance
-      pend = mxx::block_decompose_partitions(start, pend, end);
-      MP_TIMER_END_SECTION("iteration mxx::block_decompose_partitions completed");
-    }
-    */
 
     countIterations++;
     if(!rank)
@@ -286,6 +225,288 @@ int main(int argc, char** argv)
       std::cout << "Algorithm took " << countIterations << " iteration.\n";
       std::cout << "TOTAL TIME : " << time << " ms.\n"; 
     }
+}
+
+// parallel/MPI log(D_max) implementation
+void cluster_reads_par(const std::string& filename)
+{
+  static constexpr int Pn = 1;
+  static constexpr int Pc = 2;
+  //Specify Kmer Type
+  const int kmerLength = 31;
+  typedef bliss::common::DNA AlphabetType;
+  typedef bliss::common::Kmer<kmerLength, AlphabetType, uint64_t> KmerType;
+
+  //Assuming kmer-length is less than 32
+  typedef uint64_t KmerIdType;
+
+  //Assuming read count is less than 4 Billion
+  typedef uint32_t ReadIdType;
+
+  // define k-mer and operator types
+  typedef typename std::tuple<KmerIdType, ReadIdType, ReadIdType> tuple_t;
+  typedef PartitionReduceAndMarkAsInactive<Pc, Pn, tuple_t> PartitionReducerType;
+  ActivePartitionPredicate<Pn, tuple_t> app;
+
+  // get communicaiton parameters
+  int rank, p;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &p);
+
+  if(!rank) {
+    std::cout << "Runnning with " << p << " processors.\n"; 
+    std::cout << "Filename : " <<  filename << "\n"; 
+  }
+
+  timer t;
+  double startTime = t.get_ms();
+
+  /*
+   * Indices inside tuple will go like this:
+   * 0 : KmerId
+   * 1 : P_new
+   * 2 : P_old
+   */
+
+  //MP_TIMER_START();
+  MP_TIMER_START();
+
+  // Populate localVector for each rank and return the vector with all the tuples
+  std::vector<tuple_t> localVector;
+  generateReadKmerVector<KmerType, AlphabetType, ReadIdType> (filename, localVector, MPI_COMM_WORLD);
+
+  MP_TIMER_END_SECTION("Read data from disk");
+
+
+  // re-distirbute vector into equal block partition
+  //mxx::block_decompose(localVector);
+
+  assert(localVector.size() > 0);
+  auto start = localVector.begin();
+  auto end = localVector.end();
+  auto pend = end;
+
+
+  //Sort tuples by KmerId
+  bool keepGoing = true;
+  int countIterations = 0;
+  // sort by k-mer is first step (and then never again done)
+
+  // Pc == Pn
+
+  // sort by k-mers and update Pn
+  mxx::sort(start, pend, layer_comparator<0, tuple_t>(), MPI_COMM_WORLD, false);
+  // order of template params:
+  // template <uint8_t keyLayer, uint8_t reductionLayer, uint8_t resultLayer, typename T>
+  /*
+   * Indices inside tuple will go like this:
+   * 0 : KmerId
+   * 1 : P_new
+   * 2 : P_old
+   */
+  typedef KmerReduceAndMarkAsInactive<0, Pc, Pn, tuple_t> KmerReducerType;
+  KmerReducerType r1;
+  r1(start, pend, MPI_COMM_WORLD);
+  MP_TIMER_END_SECTION("iteration KMER phase completed");
+
+  // gather everything (for sequential test)
+  //std::vector<tuple_t> globalvec = mxx::gatherv(localVec);
+
+  // remove the MAX thing after k-mer reduce:
+  // TODO: own k-mer reduction function!
+  for (auto it = localVector.begin(); it != localVector.end(); ++it)
+  {
+    if (std::get<Pn>(*it) >= std::numeric_limits<int>::max()-Pn)
+      std::get<Pn>(*it) = std::get<Pc>(*it);
+  }
+
+  while (keepGoing) {
+    mxx::sort(localVector.begin(), localVector.end(), // TODO: only active partitions
+        [](const tuple_t& x, const tuple_t&y){
+        return (std::get<Pc>(x) < std::get<Pc>(y)
+            || (std::get<Pc>(x) == std::get<Pc>(y)
+            && std::get<Pn>(x) < std::get<Pn>(y)));
+        }, MPI_COMM_WORLD, false);
+    MP_TIMER_END_SECTION("mxx::sort");
+
+    layer_comparator<Pc, tuple_t> pc_comp;
+
+    // scan and find new p
+    auto begin = localVector.begin();
+    auto end = localVector.end();
+    std::vector<tuple_t> newtuples;
+    bool done = true;
+
+    // find last bucket start and send across boundaries!
+    tuple_t last_min = *(end-1);
+    last_min = *std::lower_bound(begin, end, last_min, pc_comp);
+    // for each processor, get the first element of the last most element
+    tuple_t prev_min = mxx::exscan(last_min,
+        [](const tuple_t& x, const tuple_t& y){
+          // return max Pc, if equal, return min Pn
+          if (std::get<Pc>(x) < std::get<Pc>(y) ||
+            (std::get<Pc>(x) == std::get<Pc>(y)
+             && std::get<Pn>(x) > std::get<Pn>(y)))
+          return y;
+          else return x;}
+        , MPI_COMM_WORLD);
+    tuple_t prev_el = mxx::right_shift(*(end-1), MPI_COMM_WORLD);
+
+    // get the next element
+    tuple_t first_max = *(std::upper_bound(begin, end, *begin, pc_comp)-1);
+    tuple_t next_max = mxx::reverse_exscan(first_max,
+        [](const tuple_t& x, const tuple_t& y){
+          // return min Pc, if equal, return max Pn
+          if (std::get<Pc>(x) > std::get<Pc>(y) ||
+            (std::get<Pc>(x) == std::get<Pc>(y)
+             && std::get<Pn>(x) < std::get<Pn>(y)))
+          return y;
+          else return x;}
+        , MPI_COMM_WORLD);
+    tuple_t next_el = mxx::left_shift(*begin, MPI_COMM_WORLD);
+
+    MP_TIMER_END_SECTION("reductions");
+    // for each range/bucket
+    //
+    // exchange bucket min/start !?
+    //
+    for(; begin != end; ) {
+      // find bucket (linearly)
+      auto eqr = findRange(begin, end, *begin, pc_comp);
+      assert(eqr.first == begin);
+
+      // get smallest Pn in bucket
+      auto min_pn = std::get<Pn>(*eqr.first);
+      if (rank > 0 && std::get<Pc>(prev_min) == std::get<Pc>(*eqr.first)) {
+        // first bucket and it starts on the processor to the left
+        min_pn = std::get<Pn>(prev_min);
+      }
+
+      // get largest Pn in bucket
+      auto max_pn = std::get<Pn>(*(eqr.second-1));
+      if (rank < p-1 && std::get<Pc>(next_max) == std::get<Pc>(*eqr.first)) {
+          max_pn = std::get<Pn>(next_max);
+      }
+
+      //int i = eqr.first - begin;
+      //int j = eqr.second - begin;
+      //assert(min != 676);
+
+      // remove single element buckets
+      if (eqr.first + 1 == eqr.second && (rank == 0
+          || (rank > 0 && std::get<Pc>(*eqr.first) != std::get<Pc>(prev_el)))) {
+        // single element -> no need to stick around
+        std::get<Pc>(*eqr.first) = std::get<Pn>(*eqr.first);
+        begin = eqr.second;
+        continue;
+      }
+
+      // check if all elements of the bucket are identical -> set them to
+      // their Pn.
+      // TODO: remove (inactive) if already Pn==Pc -> done
+      if (min_pn == max_pn) {
+          for (auto it = eqr.first; it != eqr.second; ++it) {
+            std::get<Pc>(*it) = std::get<Pn>(*it);
+          }
+          begin = eqr.second;
+          continue;
+      }
+
+      done = false;
+
+      // for each further element:
+      bool found_flip = false;
+      uint32_t prev_pn = std::get<Pn>(prev_el);
+      auto it = eqr.first;
+      if (rank == 0 || (rank > 0 && std::get<Pc>(*eqr.first) != std::get<Pc>(prev_el))) {
+          // skip first (since it is the min entry
+          prev_pn = min_pn;
+          it++;
+      }
+      for (; it != eqr.second; ++it) {
+        uint32_t next_pn = std::get<Pn>(*it);
+        // if duplicate or both entries are equal (Pn==Pc)
+        if (std::get<Pn>(*it) == prev_pn || std::get<Pn>(*it) == std::get<Pc>(*it)) {
+          if (!found_flip) {
+            // set flipped
+            //std::cout << "Found repeat/eq (" << std::get<1>(*it) << "," << std::get<2>(*it) << ")" << std::endl;
+            found_flip = true;
+            std::get<Pn>(*it) = std::get<Pc>(*it);
+            std::get<Pc>(*it) = min_pn;
+          } else {
+            // moves over to new partition (since duplicates are not needed)
+            std::get<Pn>(*it) = min_pn;
+            std::get<Pc>(*it) = min_pn;
+          }
+        } else {
+          // if either Pn > Pc or Pn < Pc, we flip the entry and return it
+          // to `Pn` with the new minimum
+          // update tuple, set new min and flip
+          std::swap(std::get<Pn>(*it),std::get<Pc>(*it));
+          std::get<Pn>(*it) = min_pn;
+        }
+        prev_pn = next_pn;
+      }
+
+      if (!found_flip) {
+        // TODO: don't do this for the first or last bucket...
+        // TODO: we need only one flipped per bucket
+        tuple_t t = *eqr.first;
+        std::swap(std::get<Pn>(t),std::get<Pc>(t));
+        newtuples.push_back(t);
+        std::cout << "no flip found!" << std::endl;
+      }
+
+      // next range
+      begin = eqr.second;
+    }
+    MP_TIMER_END_SECTION("local flips");
+    localVector.insert(localVector.end(), newtuples.begin(), newtuples.end());
+    MP_TIMER_END_SECTION("vector inserts");
+
+    // check if all processors are done
+    keepGoing = !mxx::test_all(done);
+    MP_TIMER_END_SECTION("check termination");
+
+    countIterations++;
+    if(!rank)
+      std::cout << "[RANK 0] : Iteration # " << countIterations <<"\n";
+  }
+
+
+#if OUTPUTTOFILE
+  //Output all (Kmer, PartitionIds) to a file in sorted order by Kmer
+  //Don't play with the 0, 2 order, this is assumed by outputCompare
+  if(!rank) std::cout << "WARNING: write to file option enabled \n";
+  writeTuplesAll<0, Pc, tuple_t>(localVector.begin(), localVector.end(), filename);
+#endif
+
+  double time = t.get_ms() - startTime;
+
+
+    if(!rank)
+    {
+      std::cout << "Algorithm took " << countIterations << " iteration.\n";
+      std::cout << "TOTAL TIME : " << time << " ms.\n"; 
+    }
+}
+
+int main(int argc, char** argv)
+{
+  // Initialize the MPI library:
+  MPI_Init(&argc, &argv);
+
+  //Specify the fileName
+  std::string filename;
+  if( argc > 1 ) {
+    filename = argv[1];
+  }
+  else {
+    std::cout << "Usage: mpirun -np 4 <executable> FASTQ_FILE\n";
+    return 1;
+  }
+
+  cluster_reads_par(filename);
 
   MPI_Finalize();
   return(0);
