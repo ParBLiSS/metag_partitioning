d38d9fe0e475e46fd79a0359ed72cb79130b9c03
r4d2 <patrick.flick@gmail.com>
2015-04-10 21:35:47 -0400

    sequential version of log(D_max) code
    Former-commit-id: 72cfb8d3d8613c2cda2d4437b68af1cf0630a91c [formerly ee1dc15c716cb4f311eb0848c505c04d0c6dd9a1]
    Former-commit-id: 15846c071d17a7b478ff426807e26ea893a83684

--- src/log_sort.cpp
+++ src/log_sort.cpp
@@ -71,6 +71,7 @@ int main(int argc, char** argv)
    * 2 : P_old
    */
 
+  //MP_TIMER_START();
   MP_TIMER_START();
 
   // Populate localVector for each rank and return the vector with all the tuples
@@ -97,7 +98,6 @@ int main(int argc, char** argv)
   // Pc == Pn
 
   // sort by k-mers and update Pn
-  MP_TIMER_START();
   mxx::sort(start, pend, layer_comparator<0, tuple_t>(), MPI_COMM_WORLD, false);
   // order of template params:
   // template <uint8_t keyLayer, uint8_t reductionLayer, uint8_t resultLayer, typename T>
@@ -120,39 +120,55 @@ int main(int argc, char** argv)
   }
 
 
+  for (auto it = localVector.begin(); it != localVector.end(); ++it)
+  {
+    if (std::get<1>(*it) >= std::numeric_limits<int>::max()-1)
+      std::get<1>(*it) = std::get<2>(*it);
+  }
 
   while (keepGoing) {
-    mxx::sort(localVector.begin(), localVector.end(), // TODO: only active partitions
+    std::sort(localVector.begin(), localVector.end(), // TODO: only active partitions
         [](const tuple_t& x, const tuple_t&y){
-        return (std::get<2>(x) < std::get<2>(x)
+        return (std::get<2>(x) < std::get<2>(y)
             || (std::get<2>(x) == std::get<2>(y)
-            && std::get<1>(x) < std::get<1>(y));
+            && std::get<1>(x) < std::get<1>(y)));
         });
-    layer_comparator<2> pc_comp;
+    layer_comparator<2, tuple_t> pc_comp;
+
 
     // scan and find new p
     auto begin = localVector.begin();
     auto end = localVector.end();
+    std::vector<tuple_t> newtuples;
     bool done = true;
     // for each range/bucket
     for(; begin != end; ) {
       // find bucket (linearly)
       auto eqr = findRange(begin, end,*begin, pc_comp);
-      auto min = *eqr.first;
+      auto min = std::get<1>(*eqr.first);
+
+      //std::cout << "####" << std::endl;
+      //std::cout << localVector << std::endl;
+
+      //int i = eqr.first - begin;
+      //int j = eqr.second - begin;
+      //assert(min != 676);
 
       if (eqr.first + 1 == eqr.second) {
-        // single element -> change partition to smaller
-        done = false;
-        // next range
+        // single element, wait till paired up!
+        //done = false;
+        std::get<2>(*eqr.first) = std::get<1>(*eqr.first); // now useless!
         begin = eqr.second;
         continue;
       }
       // TODO: compress the done elements!
       // check if all identical, then done!
       if (std::get<1>(*eqr.first) == std::get<1>(*(eqr.second-1))) {
-          for (auto it = eqr.first; it !+ eqr.second; ++it) {
+          //std::cout << "all done for bucket " << std::get<2>(*eqr.first) << std::endl;
+          for (auto it = eqr.first; it != eqr.second; ++it) {
             std::get<2>(*it) = std::get<1>(*it);
           }
+          begin = eqr.second;
           continue;
       }
 
@@ -160,10 +176,25 @@ int main(int argc, char** argv)
 
       // for each further element:
       bool found_flip = false;
-      for (auto it = eqr.first+1; it !+ eqr.second; ++it) {
-        if (std::get<1>(*it) == std::get<2>(*it)) {
+      uint32_t prev_pn = min;
+      for (auto it = eqr.first+1; it != eqr.second; ++it) {
+        uint32_t next_pn = std::get<1>(*it);
+        if (std::get<1>(*it) == prev_pn) {
+          if (!found_flip)
+          {
+            // set flipped
+            //std::cout << "Found repeat (" << std::get<1>(*it) << "," << std::get<2>(*it) << ")" << std::endl;
+            found_flip = true;
+            std::get<1>(*it) = std::get<2>(*it);
+            std::get<2>(*it) = min;
+          } else {
+            // moves over to new partition (since duplicates are not needed)
+            std::get<1>(*it) = min;
+            std::get<2>(*it) = min;
+          }
+        } else if (std::get<1>(*it) == std::get<2>(*it)) {
           if (!found_flip) {
-            std::cout << "Found eq (" << std::get<1>(*it) << "," << std::get<2>(*it) << ")" << std::endl;
+            //std::cout << "Found eq (" << std::get<1>(*it) << "," << std::get<2>(*it) << ")" << std::endl;
             found_flip = true;
             std::get<1>(*it) = std::get<2>(*it);
             std::get<2>(*it) = min;
@@ -172,34 +203,35 @@ int main(int argc, char** argv)
             std::get<1>(*it) = min;
             std::get<2>(*it) = min;
           }
+          // Pn > Pc (a flipped thing) -> unflip and return
         } else if (std::get<1>(*it) > std::get<2>(*it)) {
-          // found flipped, set min and unflip
           std::swap(std::get<1>(*it),std::get<2>(*it));
           std::get<1>(*it) = min;
         } else {
+          // Pc > Pn => new min
           // update tuple, set new min and flip
-          if (!found_flip && std::get<2>(*(it-1)) == std::get<1>(*it)) {
-            // set flipped
-            std::cout << "Found repeat (" << std::get<1>(*it) << "," << std::get<2>(*it) << ")" << std::endl;
-            found_flip = true;
-            std::get<1>(*it) = std::get<2>(*it);
-            std::get<2>(*it) = min;
-          } else {
-            std::swap(std::get<1>(*it),std::get<2>(*it));
-            std::get<1>(*it) = min;
-          }
+          std::swap(std::get<1>(*it),std::get<2>(*it));
+          std::get<1>(*it) = min;
         }
+        prev_pn = next_pn;
       }
 
       if (!found_flip) {
+        tuple_t t = *eqr.first;
+        std::swap(std::get<1>(t),std::get<2>(t));
+        newtuples.push_back(t);
+
         std::cout << "no flip found!" << std::endl;
-        exit(EXIT_FAILURE);
+        //exit(EXIT_FAILURE);
+//        assert(false);
       }
 
       // next range
-      begin = eqr.second
+      begin = eqr.second;
 
     }
+    keepGoing = !done;
+    localVector.insert(localVector.end(), newtuples.begin(), newtuples.end());
     /*
     // sort by (P_c,P_n) and update P_c via P_n
     mxx::sort(localVector.begin(), localVector.end(), // TODO: only active partitions
