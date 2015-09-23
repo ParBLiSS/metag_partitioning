036dac8673acc8968cabc4cc943b0f0c4bb891be
Chirag Jain <cjain7@gatech.edu>
2015-09-23 15:36:24 -0500

    Added comments

--- include/configPath.hpp
+++ include/configPath.hpp
@@ -10,12 +10,15 @@
 
 
 //Local disk space on each node in the distributed system for velvet (Multiple writes and reads)
+//This path should be a valid directory on the execution node
 const std::string localFS = "/local/scratch/cjain7/";
 
 //Shared space for communicating reads across ranks (Used for only one time write and read)
+//This path should be a valid directory on the execution node
 const std::string sharedFS = "/lustre/alurugroup/Chirag/Metagenome_Data/tmpDirForCode"; 
 
 //Absolute path to the project directory
+//We need to access source code folder during execution
 const std::string projSrcDir = "/work/alurugroup/chirag/Metagenomics/Partitioning/metag_partitioning/";
 
 #endif
